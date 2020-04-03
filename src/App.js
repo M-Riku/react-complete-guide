0a8aac9c9c6d4704f4cf6729185d2cfa99e61b7e
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-03 14:05:57 +0900

    state in react-hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person'
 import './App.css';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Lu", age: 25 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState,] = useState(
+    "other state"
+  );
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: "Lu", age: 18 }
       ]
     });
-  }
+  };
 
-  render() {
-    return (
-      <div className="App" >
-        <h1> Hi, world!</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      </div>
-    );
-  }
-}
+  console.log(otherState);
+
+  return (
+    <div className="App" >
+      <h1> Hi, world!</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+    </div>
+  );
+};
 
 export default App;
