f968400de996f34300ad243dd9ecff2c1d2cfd4f
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-03 15:04:10 +0900

    About transform attribute and method, and css

--- src/App.js
+++ src/App.js
@@ -5,26 +5,62 @@ import './App.css';
 class App extends Component {
   state = {
     persons: [
-      { name: "Lu", age: 25 }
+      { name: "Lu", age: 25 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 27 }
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newname) => {
     this.setState({
       persons: [
-        { name: "Lu", age: 18 }
+        { name: newname, age: 25 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 }
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Lu", age: 25 },
+        { name: event.target.value, age: 29 },
+        { name: "Stephanie", age: 27 }
       ]
     });
   }
 
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App" >
         <h1> Hi, world!</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      </div>
+        <button style={style} onClick={() => this.switchNameHandler("lulu")}>Switch Name</button>
+        <Person
+          click={this.switchNameHandler.bind(this, "lu")}
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} >
+        </Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}>
+          My Hobby: Racing!
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}>
+        </Person>
+      </div >
     );
   }
 }
