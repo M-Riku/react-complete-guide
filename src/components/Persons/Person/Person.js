7e1702cd9a2c76ea0425993894f87a8394621436
M-Riku <luminghao1994@yahoo.co.jp>
2020-04-05 15:33:56 +0900

    Dynamical app title & using hoc

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Aux from '../../hoc/Aux'
 
 import classes from './Person.module.css'
 
 const person = (props) => {
     return (
-        <div className={classes.Person}>
+        <Aux>
             <p onClick={props.clicked} > I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
-        </div>
+        </Aux>
     )
-}
+};
 
 export default person;
